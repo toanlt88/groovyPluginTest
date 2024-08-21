9b122558447a52c43aa75333a963e0789d45dcd4
ttoanle <ttoanle@webstaurantstore.com>
2024-08-21 10:58:51 -0400

    Helper class test

--- src/main/java/appTest/TcAppFunctionTest.groovy
+++ src/main/java/appTest/TcAppFunctionTest.groovy
@@ -4,4 +4,8 @@ class TcAppFunctionTest {
     static void main(String[] args) {
         print("This is the app function class")
     }
+
+    void test(String verifyString){
+        print(verifyString)
+    }
 }
--- src/main/java/appTest/test/TestAppFunction.groovy
+++ src/main/java/appTest/test/TestAppFunction.groovy
@@ -4,4 +4,8 @@ class TestAppFunction {
     static void main(String[] args) {
         print("This is the app function class")
     }
+
+    void test(String verifyString){
+        print(verifyString)
+    }
 }
