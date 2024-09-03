f6323828edede4a009ea2c9b2ba6c42391a485fd
ttoanle <ttoanle@webstaurantstore.com>
2024-09-03 15:29:48 -0400

    Check duplication

--- src/main/java/appTest/test/HelperClass.groovy
+++ src/main/java/appTest/test/HelperClass.groovy
@@ -7,42 +7,18 @@ class HelperClass {
 
     private void verify(String verifyString){
 
-        List<Integer> b = [1,2,3];
 
-        verify(
-                b[0] =1,
-                [
-                        id     : '59e71351-bfd6-4eac-b873-231152a45f88',
-                        title  : "Function check for first items 1",
-                        details: [
-                                b[0] : 1
-                        ]
-                ],
-                IssueCategory.START_UP
-        )
 
-        verify(
-                b[1] =2,
-                [
-                        id     : '59e71351-bfd6-4eac-b873-231152a45f88',
-                        title  : "Function check for first items 2",
-                        details: [
-                                b[1] : 2
-                        ]
-                ],
-                IssueCategory.START_UP
-        )
+        try{
+            List<Integer> b = [1,2,3];
 
-        verify(
-                b[2] =3,
-                [
-                        id     : '59e71351-bfd6-4eac-b873-231152a45f88',
-                        title  : "Function check for first items 3",
-                        details: [
-                                b[2] : 3
-                        ]
-                ],
-                IssueCategory.START_UP
-        )
+            switch( 0 ) {
+                case 1: break;
+                case 2: break;
+                case 2: break;          // violation
+            }
+        }catch(Exception e){
+
+        }
     }
 }
