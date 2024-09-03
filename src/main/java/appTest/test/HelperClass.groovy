765c750f0f6bc799dd0a688b1a0ef6171e922f32
ttoanle <ttoanle@webstaurantstore.com>
2024-09-03 14:00:54 -0400

    Check duplication

--- src/main/java/appTest/test/HelperClass.groovy
+++ src/main/java/appTest/test/HelperClass.groovy
@@ -6,12 +6,43 @@ class HelperClass {
     }
 
     private void verify(String verifyString){
-        def a = true
-        def parmas = a
 
-        if(a){
-            return
-        }
-        print()
+        List<Integer> b = [1,2,3];
+
+        verify(
+                b[0] =1,
+                [
+                        id     : '59e71351-bfd6-4eac-b873-231152a45f88',
+                        title  : "Function check for first items 1",
+                        details: [
+                                b[0] : 1
+                        ]
+                ],
+                IssueCategory.START_UP
+        )
+
+        verify(
+                b[1] =2,
+                [
+                        id     : '59e71351-bfd6-4eac-b873-231152a45f88',
+                        title  : "Function check for first items 2",
+                        details: [
+                                b[1] : 2
+                        ]
+                ],
+                IssueCategory.START_UP
+        )
+
+        verify(
+                b[2] =3,
+                [
+                        id     : '59e71351-bfd6-4eac-b873-231152a45f88',
+                        title  : "Function check for first items 3",
+                        details: [
+                                b[2] : 3
+                        ]
+                ],
+                IssueCategory.START_UP
+        )
     }
 }
