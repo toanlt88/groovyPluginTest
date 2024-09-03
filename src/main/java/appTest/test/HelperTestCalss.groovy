fbf98426422befdc420cc386af18f54ab351e8be
ttoanle <ttoanle@webstaurantstore.com>
2024-09-03 11:18:58 -0400

    Failer Helper class rule

---
+++ src/main/java/appTest/test/HelperTestCalss.groovy
@@ -0,0 +1,11 @@
+package appTest.test
+
+class HelperTestCalss {
+    void functionTest(){
+        verify("this is verify function")
+    }
+
+    private void verify(String verifyString){
+        print(verifyString)
+    }
+}
