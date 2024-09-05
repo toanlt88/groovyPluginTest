1debb4366ea130f85121618c42030d03e5953120
ttoanle <ttoanle@webstaurantstore.com>
2024-09-05 18:20:04 -0400

    Try new sonar rule

---
+++ src/main/java/appTest/common/ThisIsHomePageSupperLongClassNameAndNeedToRaiseIssue.groovy
@@ -0,0 +1,11 @@
+package appTest.common
+
+import app.RunWeb
+
+class ThisIsHomePageSupperLongClassNameAndNeedToRaiseIssue {
+    RunWeb r = new RunWeb();
+
+    static void main(String[] args) {
+        print("This is the app function class")
+    }
+}
